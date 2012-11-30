25c03282e4a08d4dcc3b8d61f195ba9b0552ce4a
katiasilva <katia.research@gmail.com>
2012-11-30 10:34:38 -0500

    Update glocal_registry.r

--- glocal_registry.r
+++ glocal_registry.r
@@ -1,4 +1,55 @@
 #######################################################################################
+# Example Using R with DATASUS
+#######################################################################################
+
+#####################################################################################
+#SETTING ENVIRONMENT
+#####################################################################################
+rm(list = ls())
+ls()
+
+library("ggplot2")
+library("RMySQL")
+
+#####################################################################################
+#IMPORTING DATA AND RECODING
+#####################################################################################
+
+######### if you are using MYSQL DATA BASE
+
+# Here, you can specify user authentication parameters through a MySQL default.file
+
+strPath <- "/Users/Administrator/Documents/Projects/R/Pacemaker/"
+strConf <- "susdb.cnf"
+
+# Create a Connection
+
+con <- dbConnect(MySQL(), group = "susdb" , default.file= paste( strPath , strConf , sep="") )
+
+
+sql <- "select  N_AIH, ANO_CMPT , MES_CMPT , DT_INTER, DT_SAIDA, DIAS_PERM, MUNIC_RES , 
+                tab.UF, tab.nm_municipio, CEP , NASC , IDADE, SEXO , PROC_REA , DIAG_PRINC, DIAG_SECUN , 
+                VAL_SH, VAL_SP , VAL_TOT , US_TOT 
+          from T2010 aih , censodb.tab_municipio tab 
+         where proc_rea in ( '0406010650', '0406010684')  
+           and aih.munic_res = substr(tab.cd_municipio,1,6);"
+
+df <- dbGetQuery( con , sql)
+
+head( df)
+
+## Tips to use dbAply() -- > http://goo.gl/AXhSM
+
+dbDisconnect(con)
+
+## Summary by group UF
+
+tapply(df$VAL_TOT, df$UF, summary)
+
+
+
+
+#######################################################################################
 #template_secondary_data_analysis.R is licensed under a Creative Commons Attribution - Non commercial 3.0 Unported License. see full license at the end of this file.
 #######################################################################################
 
